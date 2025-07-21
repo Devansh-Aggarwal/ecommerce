108248c553ec0adb85bd62baba0f41f3c98ea9fc
Devansh Aggarwal <131512331+Devansh-Aggarwal@users.noreply.github.com>
2025-07-21 17:11:10 +0530

    order api integrated ,cod done (11:20:00)

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoutes.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
+import orderRouter from "./routes/orderRoute.js"
 
 // App Config
 const app = express()
@@ -18,9 +19,10 @@ app.use(express.json())
 app.use(cors())
 
 //api endpoints
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
-app.use('/api/cart',cartRouter)
+app.use('/api/user',userRouter);
+app.use('/api/product',productRouter);
+app.use('/api/cart',cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get('/',(req,res)=>{
     res.send("API Working")
